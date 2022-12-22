2061b5c65c3a87d914581116f1cc0657a7e8c1bb
Prasiddha Shrestha <prasiddhastha7@gmail.com>
2022-12-22 22:13:07 +0545

    printing value in method

--- DemoReferenceAndValueTypes/Class1.cs
+++ DemoReferenceAndValueTypes/Class1.cs
@@ -12,11 +12,13 @@ namespace DemoReferenceAndValueType
         public static void Increment(int num)
         {
             num += 1;
+            Console.WriteLine(num);//check num value in Increment Method
         }
 
         public static void MakeOld(Person person)
         {
             person.age += 10;
+            Console.WriteLine(person.age);//check num value in MakeOld Method
         }
     }
     
